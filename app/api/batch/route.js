23eeaf3e693b74a0ced7a857b8149904ff9bfc26
RashedMahyoub <rashedmahyoub3@gmail.com>
2023-10-15 19:29:50 +0300

    added featrues and mssql

--- app/api/batch/route.js
+++ app/api/batch/route.js
@@ -1,9 +1,25 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import sql from "mssql";
 
-const prisma = new PrismaClient();
+export async function GET(request) {
+  await sql.connect(process.env.DATABASE_URL);
 
-export async function GET() {
-  const allBatch = await prisma.bATCH_DATA.findMany();
-  return NextResponse.json({ allBatch });
+  const { searchParams } = new URL(request.url);
+  const totalItems = await prisma.bATCH_DATA.count();
+  const totalPages = Math.ceil(totalItems / 5);
+  let pageNumber = parseInt(searchParams.get("page")) || 1;
+  const search = searchParams.get("search") || "";
+
+  if (pageNumber > totalPages) {
+    pageNumber = totalPages;
+  }
+
+  const skip = (pageNumber - 1) * 5;
+
+  const allBatch = await sql.query`select * from batch_data where WTDose like '%${search}%' offset ${skip} rows
+  FETCH NEXT 5 rows only`;
+
+  const latest = await sql.query`select * from batch_data order by TimeStamp FETCH NEXT 1 rows only`;
+
+  return NextResponse.json({ allBatch, count: totalPages, latest: latest[0] });
 }
---
+++ components/PaginatedTable/index.jsx
@@ -0,0 +1,59 @@
+"use client";
+import { Input, Table } from "antd";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+
+const PaginatedTable = ({ rowKey = "_id", columns, dataSource, total, loading, pagination = true }) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+
+  const onUpdateParams = (title, value) => {
+    const current = new URLSearchParams(Array.from(searchParams.entries())); // -> has to use this form
+
+    if (!value) {
+      current.delete(title);
+    } else {
+      current.set(title, value);
+    }
+
+    // cast to string
+    const search = current.toString();
+    const query = search ? `?${search}` : "";
+
+    router.replace(`${pathname}${query}`, { shallow: true });
+  };
+
+  return (
+    <div className="mt-4">
+      <Input
+        type="number"
+        className="mb-2"
+        placeholder="filter..."
+        onChange={(v) => onUpdateParams("search", v.target.value)}
+      />
+
+      <Table
+        size="small"
+        rowKey={rowKey}
+        columns={columns}
+        loading={loading}
+        dataSource={dataSource}
+        pagination={
+          pagination
+            ? {
+                showSizeChanger: false,
+                pageSize: 5,
+                total,
+                onChange: (page) => {
+                  onUpdateParams("page", page);
+                },
+              }
+            : false
+        }
+      />
+    </div>
+  );
+};
+
+export default PaginatedTable;
